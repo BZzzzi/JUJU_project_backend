3f4cfa1cb01b25ef66bc2d347b4f46bbe8994ba0
BZzzzi <khj23465@naver.com>
2024-07-24 16:08:52 +0900

    초기 셋팅

---
+++ src/main/java/com/juju/JUJU_project_backend/JujuProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.juju.JUJU_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JujuProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JujuProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juju/JUJU_project_backend/JujuProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juju.JUJU_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JujuProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
